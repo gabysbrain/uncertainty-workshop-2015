8ddc6ec628480e23001285b2543abf86dea70a08
Thomas Torsney-Weir <torsneyt@gmail.com>
2015-06-30 07:30:50 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -avz -e "ssh" build/ torsnet6cs@vda.univie.ac.at:/var/www/uncertainty2015/
+rsync -avz -e "ssh" --chmod=go=rX --chmod=u=rwX --delete build/ torsnet6cs@vda.univie.ac.at:/var/www/uncertainty2015/
 
