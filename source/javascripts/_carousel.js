e15ab543fc2ad198367511bd7d8b92b8c5fc127b
Thomas Torsney-Weir <torsneyt@gmail.com>
2015-06-30 10:49:26 +0200

    more carousel hacks

--- source/javascripts/_carousel.js
+++ source/javascripts/_carousel.js
@@ -4,11 +4,12 @@
 $(document).ready(function () {
   // start on a random image
   var numImgs = $('.carousel div').length;
+  var startNum = Math.floor(Math.random() * Math.max(0, numImgs-2)) + 1;
   $('.carousel').slick({
     infinite: true,
     dots: true,
-    initialSlide: Math.floor(Math.random() * numImgs),
-    slidesToShow: 1,
+    initialSlide: startNum,
+    //slidesToShow: 1,
     //centerMode: true,
     variableWidth: true
   });
